45504ea873aba0ca367211f74d118d54ea8d890a
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-03-18 15:42:15 -0600

    Initial Commit

---
+++ MyFirstSwiftUIApp/MyFirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIAppApp.swift
+//  MyFirstSwiftUIApp
+//
+//  Created by Benjamin Rojo on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
